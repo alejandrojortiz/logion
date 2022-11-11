694fb20eb9d203f9a59ad9ed83c88c3d372573f1
Alejandro Ortiz <ajortiz@princeton.edu>
2022-11-11 01:44:55 -0500

    fix

--- flask_methods.py
+++ flask_methods.py
@@ -143,7 +143,6 @@ def save_project():
     data = urllib.parse.unquote(flask.request.get_data().decode('utf-8'))
     data = urllib.parse.unquote_plus(data)
     data = data.split("&")
-    print(data)
     user_id = data[0].split("=")[1]
     text = data[1].split("=")[1]
     text_name = data[2].split("=")[1]

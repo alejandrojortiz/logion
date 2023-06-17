b7c88bafe0d8b5e400a284ff408d0a1876ff3e5e
jaysw12345 <98836706+jaysw12345@users.noreply.github.com>
2023-06-17 20:39:16 +0300

    Update server_api.py

--- server_api.py
+++ server_api.py
@@ -22,7 +22,7 @@ from sqlalchemy import insert, select, delete
     
 #FOR HOSTING
 # old_db_string = "postgresql://puycmyesqupjqt:329837e3a18d72b5b7dcbd08b3073831b3c1621e71fbca46f767a9691fe4b311@ec2-54-159-175-38.compute-1.amazonaws.com:5432/d32u7f4lie15tr"
-db_string = "postgres://gjcvtmkbtbkagu:c55c4189ba3167c280c3395af5b631e39ae7a48afbc80c394645d68ff7427b12@ec2-34-236-103-63.compute-1.amazonaws.com:5432/d5po4jpa52ptgl"
+db_string = "postgresql://gjcvtmkbtbkagu:c55c4189ba3167c280c3395af5b631e39ae7a48afbc80c394645d68ff7427b12@ec2-34-236-103-63.compute-1.amazonaws.com:5432/d5po4jpa52ptgl"
 engine = create_engine(db_string, echo=True, pool_pre_ping=True)
 
 
